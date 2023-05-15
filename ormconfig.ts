ee808146def59ee0b5c454649d210272c5023b29
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-15 23:56:30 +0100

    config file created

---
+++ ormconfig.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+
+const config: SqliteConnectionOptions = {
+  type: 'sqlite',
+  database: 'db',
+  entities: ['dist/src/**/*.entity.js'],
+};
+
+export default config;
