2559195d30a9a33ffdc75e2b8b6b5dc699aa7a35
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-16 00:46:52 +0100

    user entity modified

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,6 +5,9 @@ const config: SqliteConnectionOptions = {
   type: 'sqlite',
   database: 'db',
   entities: ['dist/src/**/*.entity.js'],
+  synchronize: false,
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli.migrationsDir: 'src/db/migrations',
 };
 
 export default config;
