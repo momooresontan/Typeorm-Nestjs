5fd1a47fc479d49bf7536aefe49edc039c626e84
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-16 23:35:53 +0100

    data-source options implemented

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,5 +1,11 @@
-import { DataSourceOptions } from 'typeorm' 
+import {  DataSource, DataSourceOptions } from 'typeorm' 
 
 export class dataSourceOptions: DataSourceOptions = {
-
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/db/mogrations/*.js'],    
 }
+
+const dataSource = new DataSource(dataSourceOptions)
+export default dataSource
